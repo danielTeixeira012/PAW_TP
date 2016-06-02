f712f864b2069afb90dc5d0a1b0c9174f7021a58
danielteixeira <danielteixeira@192.168.1.5>
2016-06-02 23:51:15 +0100

    Inserrção de ficheiro config.php e alterações no index.php

--- index.php
+++ index.php
@@ -8,10 +8,42 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link  rel="stylesheet" type="text/css" href="Application/styles/index.css">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <header id="head"><h1>Procura Emprego</h1>
+            <nav>
+                <ul>
+                    <li><a href="google.pt">+ Vista</a></li>
+                    <li><a href="google.pt">+ Vista</a></li>
+                </ul>
+            </nav>
+        </header>
+        <section id="categorias">
+            <form>
+                <input id="inf" type="checkbox"/><label for="inf">Informática</label>
+                <input id="eco" type="checkbox"/><label for="eco">Economia</label>
+                <input id="soc" type="checkbox"/><label for="soc">Sociologia</label>
+                <input id="psi" type="checkbox"/><label for="psi">Psicologia</label>
+                <input id="eng" type="checkbox"/><label for="eng">Engenharia</label>
+            </form>
+        </section>
+        <section id="ofertas">
+            <article>
+                <h3>emprego1</h3>
+                <p>Isto é um teste aos breaks para ver se isto dá break no sitio certo ás palavras</p>
+            </article>
+            <article>
+                <h3>emprego2</h3>
+                <p>Isto é um teste aos breaks para ver se isto dá break no sitio certo ás palavras</p>
+            </article>
+            <article>
+                <h3>emprego3</h3>
+                <p>Isto é um teste aos breaks para ver se isto dá break no sitio certo ás palavras</p>
+            </article>
+        </section>
+        <footer id="foot">
+            <p>&copy;2016 - Desenvolvido por Daniel Teixeira e Pedro Xavier</p>
+        </footer>
     </body>
 </html>
