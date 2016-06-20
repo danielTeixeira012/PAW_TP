b3db6ac2d209ec4448221466633b6d8322fac0ee
danielteixeira <danielteixeira@192.168.1.5>
2016-06-20 19:26:29 +0100

    difs

--- areaPessoalPrestador.php
+++ areaPessoalPrestador.php
@@ -5,6 +5,7 @@ use Config as Conf;
 
 require_once (Conf::getApplicationDatabasePath() . 'MyDataAccessPDO.php');
 require_once (Conf::getApplicationManagerPath() . 'SessionManager.php');
+require_once (Conf::getApplicationManagerPath() . 'PrestadorManager.php');
 $session = SessionManager::existSession('email');
 $tipo = SessionManager::existSession('tipoUser');
 if($session && $tipo){
@@ -14,6 +15,7 @@ if($session && $tipo){
 }else{
     header('location: index.php');
 }
+$prestadorMan =  new PrestadorManager();
 ?>
 <!DOCTYPE html>
 <!--
@@ -24,16 +26,41 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <link  rel="stylesheet" type="text/css" href="Application/styles/AreaPessoal.css">
+
         <title></title>
     </head>
-    <body>
-        <h2>Area Pessoal do Prestador <?=SessionManager::getSessionValue('email')?></h2>
-        <ul>
-            <li><a href="verPerfilPrestador.php">Ver Perfil</a></li>
-            <li><a href="ofertasTrabalhoFavoritas.php">Ofertas de Trabalho Favoritas</a></li>
-            <li><a href="ofertasTrabalhoSubmetidas.php">Ofertas de Trabalho Submetidas</a></li>
-            <li>Ofertas de Trabalho Finalizadas</li>
-        </ul>
+    <body>      
+        <?php require_once 'Application/imports/empregadorHeader.php';?>
+
+        <section id="categorias">
+            <?php
+
+           $prest = PrestadorServico::convertArrayToObject($prestadorMan->verifyEmail(SessionManager::getSessionValue('email'))[0]);
+            ?>
+            <!--Adicionar Imagem -->
+            <img id="fotoPerfil" src="Application/Resources/icons/Principal-01-256 RED.png" >
+            <p><b>Nome:</b> <?= $prest->getNome() ?></p>
+            <p><b>Email:</b> <?= $prest->getEmail() ?></p>
+            <p><b>Contato:</b> <?= $prest->getContato() ?></p>
+            <p><b>Morada:</b> <?= $prest->getMorada() ?></p>
+            <p><b>Código Postal:</b> <?= $prest->getCodPostal() ?></p>
+            <p><b>Distrito:</b> <?= $prest->getDistrito() ?></p>
+            <p><b>Concelho:</b> <?= $prest->getConcelho() ?></p>
+            <a class="button" id="editarButton" href="verPerfilPrestador.php">Editar dados...</a>
+        </section>
+        <section id="opcoes">
+            <a href="ofertasTrabalhoFavoritas.php">
+                <article>
+                    <img src="Application/Resources/icons/Add-256.png">
+                    <p>Ofertas Favoritas</p>
+                </article></a>
+            <a href="ofertasTrabalhoSubmetidas.php"><article>
+                    <img src="Application/Resources/icons/Add-256.png">
+                    <p>Ofertas Submetidas</p>
+                </article></a>
+            <p>Ofertas de Trabalho Finalizadas</p>
+        </section>
         <a href="index.php"><button>Voltar</button></a>
     </body>
 </html>
